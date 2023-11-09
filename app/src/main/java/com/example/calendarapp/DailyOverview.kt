8acb23957bba0e50f8e8c115aaa9a231dd621965
AdamK <Adam.kali@dawsoncollege.qc.ca>
2023-11-09 18:33:38 -0500

    Add column for displaying the time in a day

--- app/src/main/java/com/example/calendarapp/DailyOverview.kt
+++ app/src/main/java/com/example/calendarapp/DailyOverview.kt
@@ -1,9 +1,14 @@
 package com.example.calendarapp
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
@@ -12,6 +17,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import java.time.LocalDateTime
 
@@ -21,10 +27,13 @@ import java.time.LocalDateTime
 fun ViewPage(){
     DailyPage(modifier = Modifier, dayName = "Thursday November 9th, 2023")
 }
+
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun DailyPage(modifier: Modifier, dayName : String){
     Column{
         DaySelect(modifier = modifier,dayName = dayName)
+        Spacer(modifier = Modifier.height(10.dp))
         Text(text = "+", modifier = modifier.align(Alignment.End))
         val event1 = Event("Cooking",
             LocalDateTime.parse("2023-11-11T00:00:00"),
@@ -44,10 +53,18 @@ private fun DailyEventsTimeline(events: List<Event>,  modifier: Modifier = Modif
         .verticalScroll(rememberScrollState())
         .fillMaxWidth()) {
         Row{
+            //Column for displaying the time
             Column {
-
-
+                //Loop over all the hours in a day to display them
+                for(hour in (0..23)){
+                    Text(text = "${hour.toString()}:00",
+                        modifier = modifier
+                            .padding(12.dp),
+                        fontSize = 20.sp
+                    )
+                }
             }
+            //Column for the displaying the events
             Column {
 
             }
@@ -64,7 +81,7 @@ private fun DaySelect(modifier: Modifier = Modifier, dayName: String){
         Text(
             text = dayName,
             modifier = Modifier.align(Alignment.CenterVertically),
-            fontSize = 18.sp,
+            fontSize = 20.sp,
             fontWeight = FontWeight.Bold
         )
         Text("-->")
