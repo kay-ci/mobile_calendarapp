ab9366c4b3aea57a0eeb196258b4265d71b02b1c
jimmy xu <xujimc@gmail.com>
2023-11-13 12:31:16 -0500

    View and edit event screen without save and navigation

--- app/src/main/java/com/example/calendarapp/EventScreen.kt
+++ app/src/main/java/com/example/calendarapp/EventScreen.kt
@@ -1,15 +1,21 @@
 package com.example.calendarapp
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
@@ -22,27 +28,35 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.Blue
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import java.time.format.TextStyle
 
 @Composable
 fun EventScreen(event: Event){
     var editable: Boolean by rememberSaveable{mutableStateOf(false)}
-    Column() {
-        Button(onClick = {editable = !editable}){
-            Text("Edit")
-        }
-        if(editable){
-            TitleField(event.title)
-            TitleField("start: " + event.startTime)
-            TitleField("end: " + event.endTime)
-            TitleField("description: " + event.description)
-        }
-        else{
-            Text(event.title)
-            Text("start: " + event.startTime)
-            Text("end: " + event.endTime)
-            Text("description: " + event.description)
+    LazyColumn{
+        item{
+            Row(){
+                Button(onClick = {}){
+                    Icon(
+                        painter = painterResource(R.drawable.baseline_arrow_back_ios_24),
+                        contentDescription = "back arrow"
+                    )
+                }
+                Button(onClick = {editable = !editable},
+                    colors = if (editable) ButtonDefaults.buttonColors(Color.DarkGray) else ButtonDefaults.buttonColors()){
+                    Text("Edit")
+                }
+                Button(onClick = {}){
+                    Text("Save")
+                }
+            }
+            Field(editable, "Title", event.title)
+            Field(editable, "start", ""+event.startTime);
+            Field(editable, "end", ""+event.endTime);
+            Field(editable, "Description", event.description)
         }
     }
 }
@@ -50,18 +64,21 @@ fun EventScreen(event: Event){
 //Clean data to avoid malicious inputs
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TitleField(startingText: String){
-    var text by rememberSaveable { mutableStateOf(startingText) }
-    Row(){
+fun Field(editable: Boolean, label: String, startingValue: String){
+    var text by rememberSaveable { mutableStateOf(startingValue) }
+    Column(){
         Text(
-            text = "Title: ",
+            text = "$label: ",
             fontSize = 40.sp
         )
         TextField(
             value = text,
-            onValueChange = { text = it;},//save to database as well
-            maxLines = 1
+            onValueChange = { text = it},
+            maxLines = 1,
+            readOnly = !editable,
+            modifier = Modifier.fillMaxWidth(),
+            colors = if (editable) TextFieldDefaults.textFieldColors(containerColor = Color.White)
+                else TextFieldDefaults.textFieldColors(containerColor = Color.LightGray)
         )
-
     }
 }
