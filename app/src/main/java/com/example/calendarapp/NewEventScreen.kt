973f88e7117da5a6a65c42f92e2c59d9ff676efb
jimmy xu <xujimc@gmail.com>
2023-11-19 17:22:53 -0500

    Add back button and save button

--- app/src/main/java/com/example/calendarapp/NewEventScreen.kt
+++ app/src/main/java/com/example/calendarapp/NewEventScreen.kt
@@ -6,10 +6,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -20,8 +24,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
@@ -32,12 +38,26 @@ import java.time.LocalDateTime
 fun NewEventScreen() {
     LazyColumn(){
         item(){
-            Text("New Event",
-                modifier = Modifier.fillMaxWidth()
-                    .background(Color.Gray),
-                fontSize = 40.sp,
-                textAlign = TextAlign.Center,
-                color = Color.White)
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.background(Color.Gray)
+                    .fillMaxWidth()
+            ){
+                Button(onClick = {}){
+                    Icon(
+                        painter = painterResource(R.drawable.baseline_arrow_back_ios_24),
+                        contentDescription = "back arrow"
+                    )
+                }
+                Text("New Event",
+                    fontSize = 40.sp,
+                    textAlign = TextAlign.Center,
+                    color = Color.White)
+                Button(onClick = {}){
+                    Text("save")
+                }
+            }
         }//use TimeDialog
         item(){
             Field(true, "Title", "")
