b8c8cfc7a02956bc2a30574b94486e278a918384
AdamK <Adam.kali@dawsoncollege.qc.ca>
2023-11-19 12:06:40 -0500

    Change dailyview route to accept a value to pass

--- app/src/main/java/com/example/calendarapp/Routes.kt
+++ app/src/main/java/com/example/calendarapp/Routes.kt
@@ -2,7 +2,7 @@ package com.example.calendarapp
 
 sealed class Routes(val route: String){
     object MonthView : Routes("monthView")
-    object DailyView : Routes("dailyView")
+    object DailyView : Routes("dailyView/{selectedDate}")
     object EditEventView : Routes("editEventView")
     object CreateEventView :Routes("createEventView")
 }
