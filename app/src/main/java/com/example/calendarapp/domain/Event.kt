3a0faf16ccba5df1eb1ac6d9b5eb7947ad9bea39
AdamK <Adam.kali@dawsoncollege.qc.ca>
2023-11-27 08:57:09 -0500

    Add to event entity, primary key and column info

--- app/src/main/java/com/example/calendarapp/domain/Event.kt
+++ app/src/main/java/com/example/calendarapp/domain/Event.kt
@@ -1,14 +1,19 @@
 package com.example.calendarapp.domain
-
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.time.LocalDate
 import java.time.LocalDateTime
+
+@Entity(tableName = "events")
 data class Event(
-    var title: String,
-    val date: LocalDate,
-    val startTime: LocalDateTime,
-    var endTime: LocalDateTime,
-    var description: String,
-    var location : String
+    @PrimaryKey(autoGenerate = true)@ColumnInfo(name = "eventId") var id: Int = 0,
+    @ColumnInfo(name ="eventTitle")var title: String,
+    @ColumnInfo(name ="eventDate")val date: LocalDate,
+    @ColumnInfo(name ="eventStart")val startTime: LocalDateTime,
+    @ColumnInfo(name ="eventEnd")var endTime: LocalDateTime,
+    @ColumnInfo(name ="eventDesc")var description: String?,
+    @ColumnInfo(name ="eventLoc")var location : String?
 
     //we will use startTime as unique ID
 )
