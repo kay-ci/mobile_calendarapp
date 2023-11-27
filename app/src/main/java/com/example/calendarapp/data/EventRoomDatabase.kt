0c8d8ddd278b965f5208c12233f837404129a505
AdamK <Adam.kali@dawsoncollege.qc.ca>
2023-11-27 09:32:14 -0500

    Add room database class

---
+++ app/src/main/java/com/example/calendarapp/data/EventRoomDatabase.kt
@@ -0,0 +1,12 @@
+package com.example.calendarapp.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.calendarapp.domain.Event
+
+@Database (entities = [(Event::class)], version = 1)
+abstract class EventRoomDatabase : RoomDatabase(){
+    
+}
--- app/src/main/java/com/example/calendarapp/domain/Event.kt
+++ app/src/main/java/com/example/calendarapp/domain/Event.kt
@@ -1,4 +1,5 @@
 package com.example.calendarapp.domain
+import androidx.annotation.NonNull
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
@@ -7,13 +8,21 @@ import java.time.LocalDateTime
 
 @Entity(tableName = "events")
 data class Event(
-    @PrimaryKey(autoGenerate = true)@ColumnInfo(name = "eventId") var id: Int = 0,
-    @ColumnInfo(name ="eventTitle")var title: String,
-    @ColumnInfo(name ="eventDate")val date: LocalDate,
-    @ColumnInfo(name ="eventStart")val startTime: LocalDateTime,
-    @ColumnInfo(name ="eventEnd")var endTime: LocalDateTime,
+    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "eventId") var id: Int?,
+    @ColumnInfo(name ="eventTitle") @NonNull var title: String,
+    @ColumnInfo(name ="eventDate") @NonNull val date: LocalDate,
+    @ColumnInfo(name ="eventStart") @NonNull val startTime: LocalDateTime,
+    @ColumnInfo(name ="eventEnd") @NonNull var endTime: LocalDateTime,
     @ColumnInfo(name ="eventDesc")var description: String?,
     @ColumnInfo(name ="eventLoc")var location : String?
 
-    //we will use startTime as unique ID
-)
+){
+    constructor(
+        title: String,
+        date: LocalDate,
+        startTime: LocalDateTime,
+        endTime: LocalDateTime,
+        description: String?,
+        location: String?
+    ) : this(null, title, date, startTime, endTime, description, location)
+}
