3476ecaa6eba33244717a8e7dc306a375af87716
AdamK <Adam.kali@dawsoncollege.qc.ca>
2023-12-02 11:46:25 -0500

    Add dao test for inserting event

--- app/src/androidTest/java/com/example/calendarapp/EventDaoTest.kt
+++ app/src/androidTest/java/com/example/calendarapp/EventDaoTest.kt
@@ -7,9 +7,18 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.calendarapp.data.EventDao
 import com.example.calendarapp.data.EventRoomDatabase
 import com.example.calendarapp.domain.Event
+import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertNotNull
+import junit.framework.TestCase.assertTrue
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import org.junit.After
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runner.manipulation.Ordering
+import java.io.IOException
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.LocalTime
@@ -32,4 +41,38 @@ class EventDaoTest {
             .build()
         eventDao = eventDatabase.eventDao()
     }
+
+    @After
+    @Throws(IOException::class)
+    fun closeDb() {
+        eventDatabase.close()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testInsertIntoDB() = runBlocking {
+        // Insert an event into the database
+        eventDao.insertEvent(event1)
+
+        // Switch to the main dispatcher to observe LiveData
+        withContext(Dispatchers.Main) {
+            // Observe the LiveData
+            val allEventsLiveData = eventDao.getAllEvents()
+            allEventsLiveData.observeForever { allEvents ->
+                // Ensure the list is not empty
+                assertTrue(allEvents.isNotEmpty())
+
+                // Get the first event from the list
+                val firstEvent = allEvents[0]
+
+                // Assert that the first event matches the inserted event
+                assertEquals(event1, firstEvent)
+
+                // Stop observing to avoid leaks
+                allEventsLiveData.removeObserver {}
+            }
+        }
+    }
+
+
 }
