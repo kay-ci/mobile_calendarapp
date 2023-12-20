25b6b76cbd57f66ee0f3e35c90e52644c29d5605
kc_ <kayci.ndavila@gmail.com>
2023-12-19 19:15:21 -0500

    test getting correct month and nextMonth and previousMonth

--- app/src/androidTest/java/com/example/calendarapp/CalendarViewModelTests.kt
+++ app/src/androidTest/java/com/example/calendarapp/CalendarViewModelTests.kt
@@ -1,24 +1,71 @@
 package com.example.calendarapp
 
+import android.app.Application
+import android.icu.util.Calendar
+import android.icu.util.ULocale
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.calendarapp.presentation.viewmodel.CalendarViewModel
+import junit.framework.TestCase
+import org.junit.Before
 import org.junit.runner.RunWith
 import org.junit.Test
+import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertFalse
+import junit.framework.TestCase.assertNotSame
+import junit.framework.TestCase.assertTrue
+
 
 @RunWith(AndroidJUnit4::class)
 internal class CalendarViewModelTests {
+    private lateinit var myViewModel: CalendarViewModel
 
+    @Before
+    fun setUp() {
+        val application = ApplicationProvider.getApplicationContext<Application>()
+        myViewModel = CalendarViewModel(application)
+    }
+    @Test
     fun getMonthNumber() {
+        val expectedMonthNum1 = 1
+        val expectedMonthNum2 = 12
+
+        val actualMonth1 = myViewModel.getMonthNumber("January")
+        val actualMonth2 = myViewModel.getMonthNumber("December")
+
+
+        assertEquals(expectedMonthNum1, actualMonth1)
+        assertEquals(expectedMonthNum2, actualMonth2)
     }
 
+    @Test
     fun getMonthName() {
-    }
+        val expectedMonth1 = "January"
+        val expectedMonth2 = "December"
 
+        val actualMonth1 = myViewModel.getMonthName(1)
+        val actualMonth2 = myViewModel.getMonthName(2)
+
+        assertEquals(expectedMonth1, actualMonth1)
+        assertEquals(expectedMonth2, actualMonth2)
+    }
+    @Test
     fun nextMonth() {
-    }
+        val currentMonth = myViewModel.currentMonth.value
 
+        myViewModel.nextMonth()
+
+        assertTrue(currentMonth != myViewModel.currentMonth.value)
+    }
+    @Test
     fun previousMonth() {
-    }
+        val currentMonth = myViewModel.currentMonth.value
 
+        myViewModel.previousMonth()
+
+        assertTrue(currentMonth != myViewModel.currentMonth.value)
+    }
+    @Test
     fun getEventsForDate() {
     }
 
